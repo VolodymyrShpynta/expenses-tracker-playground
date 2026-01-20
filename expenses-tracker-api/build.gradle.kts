eff442d528bacd8d17b6f1e5e6dc3d65abdc756e
vshpynta <vladimir.shpinta@gmail.com>
2026-01-21 00:39:41 +0100

    Add more tests.

--- expenses-tracker-api/build.gradle.kts
+++ expenses-tracker-api/build.gradle.kts
@@ -1,47 +1,48 @@
 plugins {
-	alias(libs.plugins.kotlin.jvm)
-	alias(libs.plugins.kotlin.spring)
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
 description = "Expenses Tracker API"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toInt())
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(libs.versions.java.get().toInt())
+    }
 }
 
 dependencies {
-	implementation(libs.spring.boot.starter.actuator)
-	implementation(libs.spring.boot.starter.data.r2dbc)
-	implementation(libs.spring.boot.starter.webflux)
-	implementation(libs.spring.boot.starter.jdbc)
-	implementation(libs.reactor.kotlin.extensions)
-	implementation(libs.kotlin.reflect)
-	implementation(libs.kotlinx.coroutines.reactor)
-	implementation(libs.jackson.module.kotlin)
-	implementation(libs.flyway.core)
-	implementation(libs.flyway.database.postgresql)
-	runtimeOnly(libs.r2dbc.postgresql)
-	runtimeOnly(libs.postgresql)
-	testImplementation(libs.spring.boot.starter.test)
-	testImplementation(libs.spring.boot.starter.webflux.test)
-	testImplementation(libs.kotlin.test.junit5)
-	testImplementation(libs.kotlinx.coroutines.test)
-	testImplementation(libs.spring.boot.testcontainers)
-	testImplementation(libs.testcontainers.postgresql)
-	testImplementation(libs.testcontainers.r2dbc)
-	testRuntimeOnly(libs.junit.platform.launcher)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.data.r2dbc)
+    implementation(libs.spring.boot.starter.webflux)
+    implementation(libs.spring.boot.starter.jdbc)
+    implementation(libs.reactor.kotlin.extensions)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlinx.coroutines.reactor)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.flyway.core)
+    implementation(libs.flyway.database.postgresql)
+    runtimeOnly(libs.r2dbc.postgresql)
+    runtimeOnly(libs.postgresql)
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.spring.boot.starter.webflux.test)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.spring.boot.testcontainers)
+    testImplementation(libs.testcontainers.postgresql)
+    testImplementation(libs.testcontainers.r2dbc)
+    testImplementation(libs.mockito.kotlin)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
